69c0cdd58a33cb5b4a747a5c32a41e219aee7e71
aldinmasbro <aldin092gmail.com>
2024-01-23 09:23:43 +0800

    fic11-add-user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        // $users = \App\Models\User::paginate(10);
+        $users = DB::table('users')
+        ->when($request->input('name'), function ($query, $name) {
+            return $query->where('name', 'like', '%' . $name . '%');
+        })
+        ->orderBy('id', 'desc')
+        ->paginate(10);
+    return view('pages.users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        //cara mengecek error perintah di bawah ini
+        //dd($request->all());
+
+        $data = $request->all();
+        $data['password'] = Hash::make($request->password);
+        \App\Models\User::create($data);
+        return redirect()->route('user.index')->with('success', 'User succesfully created');
+
+    }
+
+}
